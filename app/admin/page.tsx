99fff20b75e044719ae19bc145b7c36d8a4fb1a2
amitoj <amitoj.mehta@gmail.com>
2026-04-15 22:14:22 +0530

    added admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,18 +1,52 @@
+"use client";
 import { data } from "@/constants/data";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 const AdminPage = () => {
-  const al = alert("Enter admin password");
-  if (al != process.env.ADMIN_PASS) return (window.location.href = "/");
-  return (
-    <section className="py-6 px-4 h-screen w-full">
-      <h1>All available users</h1>
-      <ul>
-        {data.map(({ id }) => (
-          <li key={id}>{id}</li>
-        ))}
-      </ul>
-    </section>
-  );
+  const [password, setPassword] = useState<string>("");
+  const [admin, setAdmin] = useState<boolean>(false);
+
+  const handlePasswordValidation = () => {
+    if (password === "amitoj123") {
+      setAdmin(true);
+      toast.success("Validated successfully");
+    } else {
+      toast.error("Failed to validate");
+    }
+  };
+
+  if (admin === false) {
+    return (
+      <div className="py-18 px-4 h-screen w-full">
+        <input
+          type="password"
+          value={password}
+          placeholder="enter password"
+          className="bg-gray-200 rounded shadow px-2 py-1"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          onClick={handlePasswordValidation}
+          className="btn btn-primary ml-2"
+        >
+          Validate
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <section className="py-18 px-4 h-screen w-full">
+        <h1>All available users</h1>
+        <ul>
+          {data.map(({ id }) => (
+            <li key={id}>{id}</li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
 };
 
 export default AdminPage;
